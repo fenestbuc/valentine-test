0d10dbb49cd3f4186e63b2ac23b05adbeea28dea
Vaibhav Sharma <vaibhavsharma7403@gmail.com>
2023-02-03 20:37:19 +0530

    commit

--- submit.php
+++ submit.php
@@ -1,33 +1,35 @@
 <?php
-    // Get the data from the form
-    $comfortFood = $_POST['comfort_food'];
-    $quote = $_POST['quote'];
-    $webseries = $_POST['webseries'];
-    $gender = $_POST['gender'];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $comfortFood = $_POST['comfortFood'];
+  $quote = $_POST['quote'];
+  $webseries = $_POST['webseries'];
+  $genderPreference = $_POST['genderPreference'];
 
-    // Connect to the database
-    $servername = "localhost";
-    $username = "your_username";
-    $password = "your_password";
-    $dbname = "ashoka_valentines_mixer";
+  // Connect to the database
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "ashoka_valentines";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+  // Create connection
+  $conn = new mysqli($servername
+, $username, $password, $dbname);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
 
-    // Insert the data into the database
-    $sql = "INSERT INTO responses (comfort_food, quote, webseries, gender)
-    VALUES ('$comfortFood', '$quote', '$webseries', '$gender')";
+  // Insert data into the database
+  $sql = "INSERT INTO users (comfortFood, quote, webseries, genderPreference)
+  VALUES ('$comfortFood', '$quote', '$webseries', '$genderPreference');";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Your responses have been submitted successfully. We will match you with someone special soon!";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+  if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 
-    $conn->close();
-?> 
+  $conn->close();
+}
+?>
